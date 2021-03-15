e3e5ebfc830fb857bed1a330f50f843d6d4808c1
arjunlab <2038@tkmce.ac.in>
2021-03-15 11:40:26 +0530

    stack implementation using array updated

--- Data Structures/LAB PROGRAMS/2Stack/2Stack implementation using Array/Stack implementation using Array.c
+++ Data Structures/LAB PROGRAMS/2Stack/2Stack implementation using Array/Stack implementation using Array.c
@@ -1,75 +1,74 @@
 #include<stdio.h>
 #include<stdlib.h>
-#define N 5
-int top=-1,s[N];
-void push();
+# define N 5
 void pop();
+void push();
 void display();
+int top=-1;
+int a[N];
 void main()
 {
-  int c;
-  while(1)
-   { 
-  printf("Operations of Stack\n"); 
-  printf("\n1.push\n2.Pop\n3.Display\n4.Exit\n");
-  printf("\nEnter the choice\n");
-  scanf("%d",&c);
-  switch(c)
-        {
-         case 1: push();
-                 break;
-         case 2: pop();
-                 break;
-         case 3: display();
-                 break;
-         case 4: exit(0);
-         
-        default: printf("\n Entered choice is wrong\n"); 
-        }
-     }
+int choice;
+while(1)
+{
+printf("\nThe Operations in stack\n");
+printf("\n1.Push\n2.Pop\n3.Display\n4.Exit\n");
+printf("\n Enter Your Choice\n");
+scanf("%d",&choice);
+switch(choice)
+{
+case 1:push();
+       break;
+case 2:pop();
+       break;
+case 3:display();
+       break;
+case 4:exit(0);
+       break;
+default:printf("Entered Choice is Wrong,Please Re-enter\n"); 
+} 
 }
- 
+}
 void push()
-  {
-  int i;
-  if(top==N-1)
-    {
-     printf("The stack is full\n");
-    }
-  else
-    { 
-     printf("Enter the element want to push\n");
-     scanf("%d",&i);
-     top=top+1;
-     s[top]=i;
-    }
-  }
-void pop()  
-  {
-  if(top==-1)
-    {
-    printf("The stack is empty\n");
-    }
-  else
-    {   
-     printf("\nThe poped element is %d\n",s[top]);
-     top=top-1;
-    }
-   }
-
+{
+int item;
+if(top==N-1)
+{
+printf("\nStack is Full\n");
+}
+else
+{
+top++;
+printf("Enter the Element Want to push\n");
+scanf("%d",&item);
+a[top]=item;
+}
+}
+void pop()
+{
+if(a[top]==-1)
+{
+printf("The Stack is Empty\n");
+}
+else
+{
+printf("The Poped Elementt is %d \n ",a[top]);
+top--;
+}
+}
 void display()
-   {
-   int x;
-   if(top==-1)
-    {
-    printf("The stack is empty\n");
-    }
-  else
-  {
-   for(x=top;x>=0;x--)
-     {
-     printf("%d  ",s[x]);
-     }
-   }
-   }
-
+{
+int i;
+if(a[top]==-1)
+{
+printf("The Stack is Empty\n");
+}
+else
+printf("The Elemnts of Stack are \n");
+{
+for(i=top;i>-1;i--)
+{
+printf("%d\t",a[i]);
+}
+}
+}
